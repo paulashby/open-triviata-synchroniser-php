baca95b0bcaf387984e6b203c92970453d87b998
Paul Ashby <paul@primitive.co>
2022-07-19 14:48:52 +0100

    Use connector class for API and Database connections, add while loop to process data

--- public/scraper.php
+++ public/scraper.php
@@ -1,13 +1,9 @@
 <?php
 
-include_once realpath(__DIR__ . "/../") . "/config/INI.php";
-include_once realpath(__DIR__ . "/../") . "/config/Database.php";
-include_once realpath(__DIR__ . "/../") . "/config/Token.php";
-include_once realpath(__DIR__ . "/../") . "/config/API.php";
+include_once realpath(__DIR__ . "/../") . "/config/Connector.php";
+include_once realpath(__DIR__ . "/../") . "/utilities/ErrorLogger.php";
+include_once realpath(__DIR__ . "/../") . "/models/Categories.php";
 include_once realpath(__DIR__ . "/../") . "/models/Questions.php";
-include_once realpath(__DIR__ . "/../") . "/interactions/ResponseProcessor.php";
-
-$ini_file = realpath(__DIR__ . "/../") . "/apiconfig.ini";
 
 $new_token = true;
 
@@ -23,39 +19,25 @@ if (isset($argc) && $argc > 1) {
 	}
 }
 
-// Instantiate ini for interactions with scraper config file
-$config = new INI($ini_file);
-$response_processor = new ResponseProcessor();
-$token = new Token();
-$api = new API($config, "https://opentdb.com/", $response_processor, $token);
-$database = new Database($ini_file);
+// Log any error then die
+$error_logger = new ErrorLogger();
 
-// API initalises $response_processor and $token
-$api->initialise();
-$db = $database->connect();
+// Access to Database and API
+$connector = new Connector();
 
-// Going to switch to call_user_func for callbacks so we can also pass in the callback function's class
-// The function call is
-// call_user_func(array($api, $req_details['callback']), $api_data, $req_details);
-// So if we use an array in the $req_details array, we can pass that directly
+// Determine which categories are out of sync with Open Trivia API
+$categories = new Categories($connector);
 
-// Instantiated here just for mocked req_details - but may do this anyway
-// https://stackoverflow.com/questions/15444215/php-passing-an-instance-of-a-class-to-another-class
-$questions = new Questions($db);
+// Start with the next incomplete category
+$unsynced_category = $categories->nextUnsynced();
 
-// Mock request for dev
-$mock_request = array(
-	// Callback is an array which will be passed directly by API class as first arg of call_user_function()
-	'callback'=>array($questions, 'testCallback'),
-	'endpoint'=>'api.php',
-	'parameters'=>array(
-		'category'	=> 15,
-		'amount'	=> 50
-	)
-);
+while ($unsynced_category) {
+	
+	$syncing_category = $unsynced_category->synchronise();
+	$unsynced_category = $categories->nextUnsynced($syncing_category);
+}
 
-$response = $api->request($mock_request);
-var_dump($response);
-
+error_log("SUCCESS: all questions have been processed :)");
+die();
 
 
